dc7aad646da451d964aaa38795af57d5d9c10252
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-21 22:36:14 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -19,7 +19,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   type        = list(string)
   description = "List of Availability Zones for subnets"
-  default     = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "database_name" {
